5d0632d23651da8697db5bf5df3bc640999cfbfa
Matt Insler <matt.insler@gmail.com>
2013-09-11 13:54:25 -0700

    upgraded awesomebox.node to work with 0.8.x

--- dist/commands/hidden.js
+++ dist/commands/hidden.js
@@ -1,5 +1,9 @@
 (function() {
-  var config;
+  var chalk, config, moment;
+
+  chalk = require('chalk');
+
+  moment = require('moment');
 
   config = require('../config');
 
@@ -40,15 +44,58 @@
       box = box_config.get('id');
     }
     return client.box(box).versions.list(function(err, versions) {
-      var line, _i, _len, _ref;
+      var line, lines, msg, v, x, _i, _j, _len, _ref;
       if (err != null) {
         return callback(err);
       }
-      _ref = JSON.stringify(versions, null, 2).split('\n');
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        line = _ref[_i];
-        _this.log(line);
+      versions = versions.sort(function(lhs, rhs) {
+        return lhs.number > rhs.number;
+      });
+      for (x = _i = 0, _ref = versions.length; 0 <= _ref ? _i < _ref : _i > _ref; x = 0 <= _ref ? ++_i : --_i) {
+        v = versions[x];
+        msg = v.message;
+        lines = [msg.slice(0, 60)];
+        while ((msg = msg.slice(60)) !== '') {
+          lines.push(msg.slice(0, 60));
+        }
+        if (x > 0) {
+          _this.log('');
+        }
+        _this.log("" + v.name + "                                     " + (moment(v.created_at).format('YYYY-MM-DD hh:mm:ss Z')));
+        for (_j = 0, _len = lines.length; _j < _len; _j++) {
+          line = lines[_j];
+          _this.log(chalk.yellow(line));
+        }
+        _this.log(chalk.gray('http://' + v.domain));
       }
+      return callback();
+    });
+  };
+
+  exports.open = function(box, version, callback) {
+    var box_config, client,
+      _this = this;
+    client = this.client.keyed();
+    if (client == null) {
+      return callback(errors.unauthorized());
+    }
+    if (typeof version === 'function') {
+      callback = version;
+      version = box;
+      box_config = config(process.cwd() + '/.awesomebox');
+      if (box_config.get('id') == null) {
+        return callback(new Error('Please specify a box name'));
+      }
+      box = box_config.get('id');
+    }
+    return client.box(box).version(version).get(function(err, version) {
+      var url;
+      if (err != null) {
+        return callback(err);
+      }
+      url = 'http://' + version.domain;
+      _this.log("Opening " + url);
+      require('open')(url);
       return callback();
     });
   };
--- lib/commands/hidden.coffee
+++ lib/commands/hidden.coffee
@@ -1,3 +1,5 @@
+chalk = require 'chalk'
+moment = require 'moment'
 config = require '../config'
 
 exports.boxes = (callback) ->
@@ -22,6 +24,40 @@ exports.versions = (box, callback) ->
   
   client.box(box).versions.list (err, versions) =>
     return callback(err) if err?
-    @log(line) for line in JSON.stringify(versions, null, 2).split('\n')
+    
+    versions = versions.sort (lhs, rhs) -> lhs.number > rhs.number
+    
+    for x in [0...versions.length]
+      v = versions[x]
+      
+      msg = v.message
+      lines = [msg.slice(0, 60)]
+      lines.push(msg.slice(0, 60)) while (msg = msg.slice(60)) isnt ''
+      
+      @log('') if x > 0
+      @log "#{v.name}                                     #{moment(v.created_at).format('YYYY-MM-DD hh:mm:ss Z')}"
+      @log(chalk.yellow(line)) for line in lines
+      @log chalk.gray('http://' + v.domain)
 
+    callback()
+
+exports.open = (box, version, callback) ->
+  client = @client.keyed()
+  return callback(errors.unauthorized()) unless client?
+  
+  if typeof version is 'function'
+    callback = version
+    version = box
+    
+    box_config = config(process.cwd() + '/.awesomebox')
+    return callback(new Error('Please specify a box name')) unless box_config.get('id')?
+    box = box_config.get('id')
+  
+  client.box(box).version(version).get (err, version) =>
+    return callback(err) if err?
+    
+    url = 'http://' + version.domain
+    @log "Opening #{url}"
+    require('open')(url)
+    
     callback()
